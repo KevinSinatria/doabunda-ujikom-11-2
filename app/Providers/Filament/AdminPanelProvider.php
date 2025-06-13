aeafba8c04c942413ae0b51f25aed68759ea29fa
Kevin Sinatria <kevinsinatria8@gmail.com>
2025-06-13 15:22:03 +0700

    fix: get username filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers\Filament;
 use App\Filament\Widgets\StatsOverviewWidget;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\AuthMiddleware;
+use Doctrine\DBAL\Driver\Middleware;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -59,6 +60,7 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->authMiddleware([
+                MiddlewareAuthenticate::class,
                 AuthMiddleware::class,
                 AdminMiddleware::class
             ])
